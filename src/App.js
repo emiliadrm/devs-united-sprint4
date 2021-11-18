4e0d1acea5232a236f959ee56303f3d633808dea
Emilia Rondon <emiliadrm.drawing@gmail.com>
2021-11-18 20:06:14 -0300

    adding connect to firebase, create useEffect to see the docs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, { useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
 import { firestore } from "./firebase"
 
 function App() {
+
+  useEffect(() => {
+    firestore.collection("tweets")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+      })
+    })    
+  }, []);
+
+  console.log('TESTING');
+
   return (
     <div className="App">
       <header className="App-header">
