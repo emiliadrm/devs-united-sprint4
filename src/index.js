be99a525322797394dd3c596f98b5b20702776cd
Emilia Rondon <emiliadrm.drawing@gmail.com>
2021-12-09 12:11:03 -0300

    changing browserrouter for hashrouter so I can deploy on github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom"
+//import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
 import AppProvider from './context/AppProvider';
@@ -9,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-    </BrowserRouter>,
+    </HashRouter>,
   </AppProvider>,
   </React.StrictMode>,
   document.getElementById('root')
